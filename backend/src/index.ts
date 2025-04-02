9517d28949fff9aa2ce108529341f832386546b5
lucasafonsobastos <lucasafonsob@hotmail.com>
2025-04-01 23:25:43 -0300

    Inicio

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+
+const app = express();
+
+const route = Router();
+
+app.use(express.json());
+
+route.get('/', (req: Request, res: Response) => {
+    res.json({ message: 'hello world with Typescript' })
+    
+})
+
+app.use(route)
+
+
+app.listen(3000, () => {
+    console.log("O Servidor BackEnd esta rodando...")
+})
+
